f8ac313d4a61890a427360b960b5926926166133
mou <devnull@localhost>
2009-03-22 20:15:40 +0300

    [svn r2671] Ivy refactoring

--- build-by-team-city.sh
+++ build-by-team-city.sh
@@ -2,11 +2,6 @@
 
 echo "first line of code of build-by-team-city.sh"
 
-#env > env.dump
-
-HOME=/root
-
-./ivy-scripts/publish.sh
 ./depot-scripts/build-r.sh
 ./depot-scripts/upload-r.sh yandex-bolts
 
