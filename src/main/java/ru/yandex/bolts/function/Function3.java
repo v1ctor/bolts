06b6ad31eb11c18b79e5294b9316e760a27445b2
nga <devnull@localhost>
2009-04-04 00:46:06 +0400

    [svn r2805] Function3.bind*

--- src/main/java/ru/yandex/bolts/function/Function3.java
+++ src/main/java/ru/yandex/bolts/function/Function3.java
@@ -5,4 +5,28 @@ package ru.yandex.bolts.function;
  */
 public abstract class Function3<A, B, C, R> {
     public abstract R apply(A a, B b, C c);
+    
+    public Function2<B, C, R> bind1(final A a) {
+        return new Function2<B, C, R>() {
+            public R apply(B b, C c) {
+                return Function3.this.apply(a, b, c);
+            }
+        };
+    }
+    
+    public Function2<A, C, R> bind2(final B b) {
+        return new Function2<A, C, R>() {
+            public R apply(A a, C c) {
+                return Function3.this.apply(a, b, c);
+            }
+        };
+    }
+    
+    public Function2<A, B, R> bind3(final C c) {
+        return new Function2<A, B, R>() {
+            public R apply(A a, B b) {
+                return Function3.this.apply(a, b, c);
+            }
+        };
+    }
 } //~
