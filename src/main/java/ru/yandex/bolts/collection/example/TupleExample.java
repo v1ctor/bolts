214ee2b2224317deffb646a91b191950aa57710e
Stepan Koltsov <yozh@mx1.ru>
2009-09-13 04:25:08 +0400

    fix indentation

--- src/main/java/ru/yandex/bolts/collection/example/TupleExample.java
+++ src/main/java/ru/yandex/bolts/collection/example/TupleExample.java
@@ -43,31 +43,31 @@ public class TupleExample {
 				data
 				.map(StringF.split3F(";")) // split columns 
 				.map(Tuple3.<String, String, String>get1F()); // get the first column
-		
-		assertEquals(Cf.list("Moscow", "New York", "Rio de Janeiro"), cities);
-	}
-	
-	@SuppressWarnings("unchecked")
+        
+        assertEquals(Cf.list("Moscow", "New York", "Rio de Janeiro"), cities);
+    }
+    
+    @SuppressWarnings("unchecked")
     @Test
-	public void mapNF() {
-		// very advanced topic: explains how getNF works
-		
-		ListF<String> data = Cf.list(
-				"Moscow;Russia;10.38e6",
-				"New York;USA;8.14e6",
-				"Rio de Janeiro;Brazil;6.02e6");
-		
-		ListF<Tuple3<String, String, Double>> dataParsed =
-				data
-				.map(StringF.split3F(";")) // split columns
-				.map(Tuple3.<String, String, String, Double>map3F(DoubleF.parseF())); // parse the third column
-		
-		ListF<Tuple3<String, String, Double>> expected = Cf.list(
-				Tuple3.tuple("Moscow", "Russia", 10.38e6),
-				Tuple3.tuple("New York", "USA", 8.14e6),
-				Tuple3.tuple("Rio de Janeiro", "Brazil", 6.02e6));
-		
-		assertEquals(expected, dataParsed);
-	}
-	
+    public void mapNF() {
+        // very advanced topic: explains how getNF works
+        
+        ListF<String> data = Cf.list(
+                "Moscow;Russia;10.38e6",
+                "New York;USA;8.14e6",
+                "Rio de Janeiro;Brazil;6.02e6");
+        
+        ListF<Tuple3<String, String, Double>> dataParsed =
+                data
+                .map(StringF.split3F(";")) // split columns
+                .map(Tuple3.<String, String, String, Double>map3F(DoubleF.parseF())); // parse the third column
+        
+        ListF<Tuple3<String, String, Double>> expected = Cf.list(
+                Tuple3.tuple("Moscow", "Russia", 10.38e6),
+                Tuple3.tuple("New York", "USA", 8.14e6),
+                Tuple3.tuple("Rio de Janeiro", "Brazil", 6.02e6));
+        
+        assertEquals(expected, dataParsed);
+    }
+    
 } //~
