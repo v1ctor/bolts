454688a4cd542c7f566fe85f60284d316df63cab
Stepan Koltsov <stepancheg@yandex-team.ru>
2010-02-11 16:58:04 +0300

    minor (better cast) proposed by ssytnik

--- src/main/java/ru/yandex/bolts/function/Function.java
+++ src/main/java/ru/yandex/bolts/function/Function.java
@@ -97,7 +97,7 @@ public abstract class Function<A, R> {
     /** And then null low natural comparator */
     @SuppressWarnings({"unchecked"})
     public Comparator<A> andThenNaturalComparator() {
-        return andThen((Comparator) Comparator.naturalComparator());
+        return andThen((Comparator<R>) Comparator.naturalComparator());
     }
 
     /**
