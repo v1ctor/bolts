e7d24be585a462af01f5fd24d5df299ef0dcac72
Stepan Koltsov <yozh@mx1.ru>
2010-04-05 00:39:45 +0400

    function name parser

---
+++ src/main/java/ru/yandex/bolts/function/meta/FunctionTypeTest.java
@@ -0,0 +1,22 @@
+package ru.yandex.bolts.function.meta;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ru.yandex.bolts.collection.Option;
+import ru.yandex.bolts.function.meta.FunctionType.ReturnType;
+
+/**
+ * @author Stepan Koltsov
+ */
+public class FunctionTypeTest {
+    @Test
+    public void testParse() {
+        assertEquals(Option.none(), FunctionType.parseSimpleClassName("Function1"));
+        assertEquals(Option.none(), FunctionType.parseSimpleClassName("Functio1B"));
+        assertEquals(new FunctionType(1, ReturnType.OBJECT), FunctionType.parseSimpleClassName("Function").get());
+        assertEquals(new FunctionType(3, ReturnType.BOOLEAN), FunctionType.parseSimpleClassName("Function3B").get());
+        assertEquals(new FunctionType(0, ReturnType.OBJECT), FunctionType.parseSimpleClassName("Function0").get());
+    }
+} //~
