46e34133aefb851c8738d700f9c9d9b95937f601
Stepan Koltsov <yozh@mx1.ru>
2011-06-25 02:41:10 +0400

    do not extend AbstractCollection (copy-paste it methods)

--- src/main/java/ru/yandex/bolts/collection/impl/AbstractCollectionF.java
+++ src/main/java/ru/yandex/bolts/collection/impl/AbstractCollectionF.java
@@ -1,7 +1,7 @@
 package ru.yandex.bolts.collection.impl;
 
 import java.lang.reflect.Array;
-import java.util.AbstractCollection;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
@@ -33,7 +33,179 @@ import ru.yandex.bolts.function.forhuman.Comparator;
  *
  * @author Stepan Koltsov
  */
-public abstract class AbstractCollectionF<E> extends AbstractCollection<E> implements CollectionF<E> {
+public abstract class AbstractCollectionF<E> implements CollectionF<E> {
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        Iterator<E> e = iterator();
+        if (o == null) {
+            while (e.hasNext())
+                if (e.next() == null)
+                    return true;
+        } else {
+            while (e.hasNext())
+                if (o.equals(e.next()))
+                    return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        // Estimate size of array; be prepared to see more or fewer elements
+        Object[] r = new Object[size()];
+        Iterator<E> it = iterator();
+        for (int i = 0; i < r.length; i++) {
+            if (!it.hasNext()) // fewer elements than expected
+                return Arrays.copyOf(r, i);
+            r[i] = it.next();
+        }
+        return it.hasNext() ? finishToArray(r, it) : r;
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        // Estimate size of array; be prepared to see more or fewer elements
+        int size = size();
+        T[] r = a.length >= size ? a : (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+        Iterator<E> it = iterator();
+
+        for (int i = 0; i < r.length; i++) {
+            if (!it.hasNext()) { // fewer elements than expected
+                if (a != r)
+                    return Arrays.copyOf(r, i);
+                r[i] = null; // null-terminate
+                return r;
+            }
+            r[i] = (T) it.next();
+        }
+        return it.hasNext() ? finishToArray(r, it) : r;
+    }
+
+    private static <T> T[] finishToArray(T[] r, Iterator<?> it) {
+        int i = r.length;
+        while (it.hasNext()) {
+            int cap = r.length;
+            if (i == cap) {
+                int newCap = ((cap / 2) + 1) * 3;
+                if (newCap <= cap) { // integer overflow
+                    if (cap == Integer.MAX_VALUE)
+                        throw new OutOfMemoryError("Required array size too large");
+                    newCap = Integer.MAX_VALUE;
+                }
+                r = Arrays.copyOf(r, newCap);
+            }
+            r[i++] = (T) it.next();
+        }
+        // trim if overallocated
+        return (i == r.length) ? r : Arrays.copyOf(r, i);
+    }
+
+    @Override
+    public boolean add(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        Iterator<E> e = iterator();
+        if (o == null) {
+            while (e.hasNext()) {
+                if (e.next() == null) {
+                    e.remove();
+                    return true;
+                }
+            }
+        } else {
+            while (e.hasNext()) {
+                if (o.equals(e.next())) {
+                    e.remove();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        Iterator<?> e = c.iterator();
+        while (e.hasNext())
+            if (!contains(e.next()))
+                return false;
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        boolean modified = false;
+        Iterator<? extends E> e = c.iterator();
+        while (e.hasNext()) {
+            if (add(e.next()))
+                modified = true;
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        Iterator<?> e = iterator();
+        while (e.hasNext()) {
+            if (c.contains(e.next())) {
+                e.remove();
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean modified = false;
+        Iterator<E> e = iterator();
+        while (e.hasNext()) {
+            if (!c.contains(e.next())) {
+                e.remove();
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public void clear() {
+        Iterator<E> e = iterator();
+        while (e.hasNext()) {
+            e.next();
+            e.remove();
+        }
+    }
+
+    @Override
+    public String toString() {
+        Iterator<E> i = iterator();
+        if (! i.hasNext())
+            return "[]";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (;;) {
+            E e = i.next();
+            sb.append(e == this ? "(this Collection)" : e);
+            if (! i.hasNext())
+                return sb.append(']').toString();
+            sb.append(", ");
+        }
+    }
+
+
+
     protected <B> CollectionF<B> newMutableCollection() {
         return CollectionsF.arrayList();
     }
